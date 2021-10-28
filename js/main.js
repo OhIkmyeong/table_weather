70f912ef9a5d142161e72b9b2605589310b6f625
Oh JuYeon <dpffpself@naver.com>
2021-10-28 10:37:18 +0900

    commit comment

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 import { start_making_table } from "./table.js";
 
-start_making_table('00_korea');
-start_making_table('01_temper');
-start_making_table('02_rain');
-start_making_table('03_wind');
-start_making_table('04_snow');
-start_making_table('05_air_press');
-start_making_table('06_demp');
-start_making_table('07_vapor_pressure');
-start_making_table('08_vapor');
-start_making_table('09_sunshine');
-start_making_table('10_radiation');
-start_making_table('11_cloud');
-start_making_table('12_temp_ground');
-start_making_table('13_meteorological');
-start_making_table('14_seasonal_value');
-start_making_table('15_climate_index');
-start_making_table('16_distribution');
+async function make_table_all(){
+    await start_making_table('00_korea');
+    await start_making_table('01_temper');
+    await start_making_table('02_rain');
+    await start_making_table('03_wind');
+    await start_making_table('04_snow');
+    await start_making_table('05_air_press');
+    await start_making_table('06_demp');
+    await start_making_table('07_vapor_pressure');
+    await start_making_table('08_vapor');
+    await start_making_table('09_sunshine');
+    await start_making_table('10_radiation');
+    await start_making_table('11_cloud');
+    await start_making_table('12_temp_ground');
+    await start_making_table('13_meteorological');
+    await start_making_table('14_seasonal_value');
+    await start_making_table('15_climate_index');
+    await start_making_table('16_distribution');
+}
+
+make_table_all();
